457d717b9c721419244ceb50211f6ed97a9d8d05
George <georgehadjiantonis@hotmail.com>
2019-09-25 21:53:30 +0300

    Classes definitions

---
+++ src/simpleNeuralNetwork/Layer.java
@@ -0,0 +1,5 @@
+package simpleNeuralNetwork;
+
+public class Layer {
+
+}
---
+++ src/simpleNeuralNetwork/SimpleNeuralNetworkDriver.java
@@ -0,0 +1,7 @@
+package simpleNeuralNetwork;
+
+public class SimpleNeuralNetworkDriver {
+	public static void main(String[] args) { 
+		
+	} 
+}
